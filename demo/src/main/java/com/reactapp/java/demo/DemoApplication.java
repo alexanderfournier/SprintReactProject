f00c83956f8900612754791b8efabfead8a71c2c
alexander.fournier <alexander.fournier@nasm.org>
2018-03-18 18:00:29 -0700

    redo

--- demo/src/main/java/com/reactapp/java/demo/DemoApplication.java
+++ demo/src/main/java/com/reactapp/java/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.reactapp.java.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication is a combination of @ComponenetScan, @EnableAutoConfiguration, @Configuration
+ * @author alexander.fournier
+ *
+ */
+
 @SpringBootApplication
 public class DemoApplication {
 
